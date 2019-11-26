3dc07a83cbc8de46fec64f991198e11c68ede605
ShayWellington <53454314+ShayWellington@users.noreply.github.com>
2019-11-26 15:24:22 +1300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,34 @@
+console.log('100$ Pure Accommodation');
+
+// Swiper Carousel JS
+var swiper = new Swiper('.swiper-container', {
+      spaceBetween: 30,
+      centeredSlides: true,
+      autoplay: {
+        delay: 2500,
+        disableOnInteraction: false,
+      },
+      pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+      },
+      navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+      },
+    });
+
+var locations = [
+  {
+    name: ,
+    address: ,
+    lat: ,
+    long: ,
+    type: ,
+    guestMax: ,
+    guestMin: ,
+    costPN: ,
+    minNights: ,
+    maxNights: ,
+  }
+]
